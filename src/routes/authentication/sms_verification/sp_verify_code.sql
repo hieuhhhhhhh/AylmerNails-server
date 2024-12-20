6501c58c9f9b1e6340c2ae1b508de03082e331fe
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-20 16:51:32 -0500

    cont

--- src/routes/authentication/sms_verification/sp_verify_code.sql
+++ src/routes/authentication/sms_verification/sp_verify_code.sql
@@ -1,53 +1,54 @@
 DROP PROCEDURE IF EXISTS sp_verify_code;
 
 CREATE PROCEDURE sp_verify_code(
-    IN phonenum VARCHAR(15),
-    IN code VARCHAR(4),
-    IN lifespan INT
+    IN _phone_number VARCHAR(15),
+    IN _code VARCHAR(4),
+    IN _lifespan INT
 )
 sp:BEGIN
     -- Placeholders for data from table
-    DECLARE _code VARCHAR(4);
-    DECLARE _attempts_left INT;
-    DECLARE _created_at BIGINT;
+    DECLARE code_ VARCHAR(4);
+    DECLARE attempts_left_ INT;
+    DECLARE created_at_ BIGINT;
 
     -- Decrement the attempts left
     UPDATE sms_verify_codes
     SET attempts_left = attempts_left - 1
-    WHERE phone_number = phonenum;
+    WHERE phone_number = _phone_number;
 
     -- Get the current timestamp and record details
-    SELECT code, attempts_left, created_at INTO _code, _attempts_left, _created_at
+    SELECT code, attempts_left, created_at INTO code_, attempts_left_, created_at_
     FROM sms_verify_codes
-    WHERE phone_number = phonenum;
+    WHERE phone_number = _phone_number;
 
     -- If the phone number doesn't exist or other failure conditions
-    IF _code IS NULL THEN
+    IF code_ IS NULL THEN
         -- Return the failure result set
         SELECT FALSE AS success, 'Code not found, please try again' AS msg;
         LEAVE sp;
     END IF;
 
-    -- Check if the code is correct
-    IF _code != code THEN
+    -- Check if the code has expired (using the created_at time + _lifespan)
+    IF (UNIX_TIMESTAMP() - created_at_) > _lifespan THEN
         -- Return the failure result set
-        SELECT FALSE AS success, 'Incorrect code, please try again' AS msg;
+        SELECT FALSE AS success, 'Code has expired, please request a new code' AS msg;
         LEAVE sp;
     END IF;
 
     -- Check if there are no remaining attempts
-    IF _attempts_left < 0 THEN
+    IF attempts_left_ < 0 THEN
         -- Return the failure result set
         SELECT FALSE AS success, 'No attempts left, please request a new code' AS msg;
         LEAVE sp;
     END IF;
 
-    -- Check if the code has expired (using the created_at time + lifespan)
-    IF (UNIX_TIMESTAMP() - _created_at) > lifespan THEN
+    -- Check if the code is correct
+    IF code_ != _code THEN
         -- Return the failure result set
-        SELECT FALSE AS success, 'Code has expired, please request a new code' AS msg;
+        SELECT FALSE AS success, 'Incorrect code, please try again' AS msg;
         LEAVE sp;
     END IF;
+
 
     -- If everything is valid, return success and delete the record
     SELECT TRUE AS success, 'Verification successful.' AS msg;
