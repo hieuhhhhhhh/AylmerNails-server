d24ca6fd54c1e52cf00ed349e4758c9cc7838dcb
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-20 15:46:12 -0500

    cont

--- src/routes/authentication/sms_verification/sp_store_code.sql
+++ src/routes/authentication/sms_verification/sp_store_code.sql
@@ -2,27 +2,27 @@ DROP PROCEDURE IF EXISTS s_store_code;
 
 CREATE PROCEDURE s_store_code (
     IN _phone_number VARCHAR(15),
-    IN _password VARCHAR(60),
+    IN _new_password VARCHAR(60),
     IN _code VARCHAR(4),
     IN _attempts_left INT
 )
 BEGIN
     INSERT INTO sms_verify_codes (
         phone_number,
-        hashed_password,
+        new_password,
         code,
         attempts_left,
         created_at
     )
     VALUES (
         _phone_number,
-        _password, 
+        _new_password, 
         _code,
         _attempts_left,
         UNIX_TIMESTAMP()
     )
     ON DUPLICATE KEY UPDATE
-        hashed_password = VALUES(hashed_password), 
+        new_password = VALUES(new_password), 
         code = VALUES(code),
         attempts_left = VALUES(attempts_left),
         created_at = VALUES(created_at);
