9d33378e35dd2e7ebb7c2159f16ebfa00c5478cc
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-21 16:57:37 -0500

    cont

--- src/routes/authentication/login/continue_session.py
+++ src/routes/authentication/login/continue_session.py
@@ -4,24 +4,23 @@ from src.mysql.call_sp import call_sp
 
 
 def continue_session(token):
-    # fetch salt from env
-    TOKEN_SALT = current_app.config["TOKEN_SALT"]
-
     # Initialize Hashids with a salt and optional configuration
-    hashids = Hashids(salt=TOKEN_SALT, min_length=20)
+    hashids = Hashids(salt=current_app.config["TOKEN_SALT"], min_length=20)
 
     # Decode token get the required information
     session_id, session_salt = hashids.decode(token)
 
     # Process the retrieved data
-    session_id, session_salt = call_sp("sp_continue_session", session_id, session_salt)
+    res = call_sp("sp_continue_session", session_id, session_salt)
 
-    # The procedure should return new salt if not the token is invalid
-    if session_id is None or session_salt is None:
-        return jsonify({"message": "Invalid token"}), 404
-
-    # if the token is valid return client the refreshed token
-    else:
+    # The procedure should return new salt
+    if len(res):
         # Encode session ID and new salt to generate new token
+        session_id, session_salt = res[0]
         token = hashids.encode(session_id, session_salt)
+
         return jsonify({"token": token}), 200
+
+    # if the token is invalid return client the refreshed token
+    else:
+        return jsonify({"message": "Invalid token"}), 404
