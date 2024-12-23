8de5ced39d896d35037837d8ec4296c20d681b33
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-23 04:44:54 -0500

    c

--- src/routes/helpers/response_with_token.py
+++ src/routes/helpers/response_with_token.py
@@ -1,8 +1,18 @@
 from flask import make_response
+from datetime import datetime, timedelta, timezone
+
+# Set the how long the cookie should be in the client's browser (in seconds)
+COOKIE_AGE = 6 * 30 * 24 * 60 * 60  # 6 months
 
 
 def response_with_token(body, status, token):
     response = make_response(body, status)
-    response.set_cookie("TOKEN", token)
+
+    # Make the current datetime UTC and timezone-aware
+    expires = datetime.now(timezone.utc) + timedelta(seconds=COOKIE_AGE)
+
+    response.set_cookie(
+        "TOKEN", token, expires=expires, samesite="None", httponly=True, secure=True
+    )
 
     return response
