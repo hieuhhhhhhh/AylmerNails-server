34ce95e4d987c3efd0cbd7e0edd44058d659de3e
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-26 23:39:51 -0500

    c

--- src/mysql/tables/employees/employees.sql
+++ src/mysql/tables/employees/employees.sql
@@ -1,4 +1,9 @@
 CREATE TABLE employees(
     employee_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    alias VARCHAR(50) NOT NULL
-)
+    alias VARCHAR(50) NOT NULL,
+    first_date BIGINT UNSIGNED NOT NULL, -- the first day that this employee start working for the business
+    last_date BIGINT UNSIGNED
+);
+
+CREATE INDEX idx_first_date ON employees(first_date);
+CREATE INDEX idx_last_date ON employees(last_date);
--- src/mysql/tables/employees/schedules.sql
+++ src/mysql/tables/employees/schedules.sql
@@ -1,14 +1,13 @@
 CREATE TABLE schedules(
     schedule_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     employee_id INT UNSIGNED,
-    effective_from BIGINT NOT NULL,
-    day_of_the_week TINYINT, -- start from sunday = 0, monday = 1, tuesday = 2, ...
-    start_time INT,
-    end_time INT,
+    effective_from BIGINT NOT NULL, -- the time when this schedule takes effect
+    day_of_the_week TINYINT, -- sunday = 0, monday = 1, tuesday = 2, etc.
+    opening_time INT, -- the clock time when employee start on that day (in seconds)
+    closing_time INT,
     FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
         ON DELETE CASCADE
 );
 
--- index on employee_id -> start_time
-CREATE INDEX idx_employee_start ON schedules(employee_id, start_time);
-
+-- index on employee_id -> day_of_the_week -> effective_from
+CREATE INDEX idx_employee_id_day_of_the_week_effective_from ON schedules(employee_id, day_of_the_week, effective_from);
