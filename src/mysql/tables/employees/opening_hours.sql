ebe8dbdc7c36e2e4025729b238214768f7f06894
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-27 18:22:48 -0500

    c

---
+++ src/mysql/tables/employees/opening_hours.sql
@@ -0,0 +1,9 @@
+CREATE TABLE opening_hours (
+    schedule_begin BIGINT NOT NULL, -- the time when this schedule takes effect
+    day_of_the_week TINYINT, -- sunday = 0, monday = 1, tuesday = 2, etc.
+    opening_time INT, -- the clock time when employee starts on that day (in seconds)
+    closing_time INT, -- the clock time when employee stops on that day (in seconds)
+    FOREIGN KEY (schedule_begin) REFERENCES schedules(schedule_begin)
+        ON DELETE CASCADE,
+    PRIMARY KEY (schedule_begin, day_of_the_week) -- composite primary key
+);
