4e5990fa6d896d2b2159cf68b4b5394f1a688369
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-29 17:17:56 -0500

    c

--- src/routes/services/services/sql/sp_add_service.sql
+++ src/routes/services/services/sql/sp_add_service.sql
@@ -1,5 +1,15 @@
 DROP PROCEDURE IF EXISTS sp_add_service;
 
 CREATE PROCEDURE sp_add_service(
-    IN name VARCHAR(50)
+    IN _name VARCHAR(50),
+    IN _category_id INT UNSIGNED,
 )
+BEGIN
+    -- Insert the new category into the categories table
+    INSERT INTO categories (name, category_id)
+    VALUES (_name, _category_id);
+    
+    -- Optionally, you could return the last inserted ID
+    SELECT LAST_INSERT_ID();
+END; 
+
