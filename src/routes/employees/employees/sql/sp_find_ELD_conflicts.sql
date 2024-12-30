e314c3e1b960e8fbde6e7908fef1d99b81b3417c
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-29 21:49:09 -0500

    c

--- src/routes/employees/employees/sql/sp_find_ELD_conflicts.sql
+++ src/routes/employees/employees/sql/sp_find_ELD_conflicts.sql
@@ -1,9 +1,24 @@
--- ELD = employee's last date
 DROP PROCEDURE IF EXISTS sp_find_ELD_conflicts;
 
 CREATE PROCEDURE sp_find_ELD_conflicts(
-    IN  _employee_id INT UNSIGNED, 
+    IN _employee_id INT UNSIGNED
 )
 BEGIN
+    DECLARE last_date_ BIGINT;
 
+    -- Fetch last_date for the given employee
+    SELECT last_date
+    INTO last_date_
+    FROM employees
+    WHERE employee_id = _employee_id;
+
+    IF last_date_ IS NOT NULL THEN
+        -- Find appointments with a date greater than the employee's last_date
+        SELECT *
+        FROM appo_details
+        WHERE date > last_date_ 
+            AND employee_id = _employee_id;
+    END IF;
 END;
+
+
