83199f9a09484708c3760c7376b71ca4e797e207
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-29 22:10:35 -0500

    Update sp_find_ELD_conflicts.sql

--- src/routes/employees/employees/sql/sp_find_ELD_conflicts.sql
+++ src/routes/employees/employees/sql/sp_find_ELD_conflicts.sql
@@ -5,23 +5,32 @@ CREATE PROCEDURE sp_find_ELD_conflicts(
 )
 BEGIN
     DECLARE last_date_ BIGINT;
+    DECLARE exit HANDLER FOR SQLEXCEPTION
+        ROLLBACK;  -- Rollback if there is any error
 
-    -- Fetch last_date for the given employee
-    SELECT last_date
-    INTO last_date_
-    FROM employees
-    WHERE employee_id = _employee_id;
+    -- Start the transaction
+    START TRANSACTION;
 
-    IF last_date_ IS NOT NULL THEN
-        -- Remove all existing conflicts for the given employee before revalidating
-        DELETE FROM ELD_appo_conflicts
+        -- Fetch last_date of the given employee
+        SELECT last_date
+        INTO last_date_
+        FROM employees
         WHERE employee_id = _employee_id;
 
-        -- Insert appointments with a date greater than the employee's last_date into ELD_appo_conflicts
-        INSERT INTO ELD_appo_conflicts (appo_id, employee_id)
-        SELECT appo_id, _employee_id
-        FROM appo_details
-        WHERE date > last_date_ 
-            AND employee_id = _employee_id;
-    END IF;
+        -- Proceed only if last_date is not NULL
+        IF last_date_ IS NOT NULL THEN
+            -- Remove all existing conflicts for the given employee before revalidating
+            DELETE FROM ELD_appo_conflicts
+            WHERE employee_id = _employee_id;
+
+            -- Insert appointments with a date greater than the employee's last_date into ELD_appo_conflicts
+            INSERT INTO ELD_appo_conflicts (appo_id, employee_id)
+            SELECT appo_id, _employee_id
+            FROM appo_details
+            WHERE date > last_date_ 
+                AND employee_id = _employee_id;
+        END IF;
+
+    -- Commit the transaction if everything went well
+    COMMIT;
 END;
