34e4b0d1e3f65c5bcd94e304d721858d3bad0d17
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-01-01 20:02:22 -0500

    c

--- src/routes/services/lengths/sql/fn_find_conflicting_length.sql
+++ src/routes/services/lengths/sql/fn_find_conflicting_length.sql
@@ -13,5 +13,68 @@ CREATE FUNCTION fn_find_conflicting_length(
 RETURNS INT UNSIGNED
 DETERMINISTIC
 BEGIN
+    -- index to iterate json array
+    DECLARE i TINYINT,
 
+    -- other place holders
+    DECLARE service_length_id_ INT UNSIGNED,
+    DECLARE length_ INT,
+    DECLARE offset_ INT,
+    DECLARE AOS_id_ INT UNSIGNED,
+    DECLARE option_id_ INT UNSIGNED,
+
+    -- fetch service's default length and its id
+    SELECT service_length_id, length
+        INTO service_length_id_, length_
+        FROM service_lengths
+        WHERE service_id = _service_id
+            AND effective_from <= UNIX_TIMESTAMP()
+        ORDER BY effective_from DESC
+        LIMIT 1;
+
+    -- fetch and merge offset of employee to current length
+    SET offset_ = 0;
+    SELECT length_offset
+        INTO offset_
+        FROM SLVs
+        WHERE service_length_id = service_length_id_
+            AND employee_id = _employee_id
+        LIMIT 1;
+
+    SET length_ = length_ + offset_;
+
+    -- iterate _selected_AOSO, fetch and merge offset of every AOSO 
+    WHILE i < JSON_LENGTH(_selected_AOSO) DO 
+        -- fetch every AOS_id_
+        SET AOS_id_ = JSON_UNQUOTE(JSON_EXTRACT(_selected_AOSO, CONCAT('$[', i, ']')));
+        SET i = i + 1;
+
+        -- fetch the option id for that AOS
+        SET option_id_ = JSON_UNQUOTE(JSON_EXTRACT(_selected_AOSO, CONCAT('$[', i, ']')));
+        SET i = i + 1;
+
+        -- fetch and merge offset of that option
+        SET offset_ = 0;
+    
+        SELECT length_offset
+            INTO offset_
+            FROM AOS_options ao
+                JOIN add_on_services aos
+                ON aos.AOS_id = ao.AOS_id
+            WHERE aos.service_id = _service_id
+                AND ao.AOS_id = AOS_id_
+                AND ao.option_id = option_id_
+            LIMIT 1;
+
+        SET length_ = length_ + offset_;
+
+        -- end loop
+    END WHILE;
+
+    -- Return an service_length_id with which the appointment violates
+    IF length_ = (_end_time - _start_time) THEN
+        RETURN NULL;
+    ELSE 
+        RETURN service_length_id_;
+    END IF;
 END;
