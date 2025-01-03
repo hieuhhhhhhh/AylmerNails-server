06309eb9e18f0572e4cdf30a5c636f1f1cf51444
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-01-02 22:48:25 -0500

    C

--- src/mysql/tables/services/add_ons/add_on_services.sql
+++ src/mysql/tables/services/add_ons/add_on_services.sql
@@ -1,11 +1,11 @@
 CREATE TABLE add_on_services (
+    AOS_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     service_id INT UNSIGNED NOT NULL,
-    AOS_id INT UNSIGNED AUTO_INCREMENT,
     prompt VARCHAR(400),
-
+    
     FOREIGN KEY (service_id) REFERENCES services(service_id)
-        ON DELETE CASCADE,
-
-    PRIMARY KEY (service_id, AOS_id) -- Composite primary key
+        ON DELETE CASCADE
 );
 
+-- index on service_id -> AOS_id
+CREATE INDEX idx_service_id_AOS_id ON add_on_services(service_id, AOS_id)
