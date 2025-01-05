2e94c1b711381171acda1a5387eea1b05d5a5659
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-01-04 22:30:03 -0500

    c

--- src/routes/employees/__init__.py
+++ src/routes/employees/__init__.py
@@ -1,27 +1,44 @@
-import json
 from flask import Blueprint, request
-
-from ..helpers.unexpected_error_response import unexpected_error_response
 from src.routes.authentication.session.read_token import read_token
+from ..helpers.unexpected_error_response import unexpected_error_response
+from src.routes.employees.employees.add_employee import add_employee
+from src.routes.employees.employees.set_employee_last_date import set_employee_last_date
 
 # create blueprint (group of routes)
 employees = Blueprint("employees", __name__)
 
 
-# @employees.route("/add_employee", methods=["POST"])
-# def request_sign_up():
-#     try:
-#         # read token
-#         session = read_token()
+@employees.route("/add_employee", methods=["POST"])
+def request_sign_up():
+    try:
+        # read token
+        session = read_token()
 
-#         # read json from request
-#         data = request.get_json()
-#         name = data.get("name")
-#         category_id = data.get("category_id")
-#         AOSs = json.dumps(data.get("AOSs"))  # convert python list to json
+        # read json from request
+        data = request.get_json()
+        alias = data.get("alias")
+        first_date = data.get("first_date")
 
-#         return add_employee(session, name, category_id, AOSs)
+        return add_employee(session, alias, first_date)
 
-#     # catch unexpected error
-#     except Exception as e:
-#         return unexpected_error_response(e)
+    # catch unexpected error
+    except Exception as e:
+        return unexpected_error_response(e)
+
+
+@employees.route("/set_employee_last_date", methods=["POST"])
+def request_sign_up():
+    try:
+        # read token
+        session = read_token()
+
+        # read json from request
+        data = request.get_json()
+        employee_id = data.get("employee_id")
+        last_date = data.get("last_date")
+
+        return set_employee_last_date(session, employee_id, last_date)
+
+    # catch unexpected error
+    except Exception as e:
+        return unexpected_error_response(e)
