53e77b601ff2b6ea7f60eb18ee8d70ea2c6421b7
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-01-06 22:00:27 -0500

    Update sp_get_DELAs_or_appo_lists.sql

--- src/routes/appointments/availability/sql/sp_get_DELAs_or_appo_lists.sql
+++ src/routes/appointments/availability/sql/sp_get_DELAs_or_appo_lists.sql
@@ -3,13 +3,17 @@ DROP PROCEDURE IF EXISTS sp_get_DELAs_or_appo_lists;
 CREATE PROCEDURE sp_get_DELAs_or_appo_lists(
     IN _date BIGINT,
     IN _service_id INT UNSIGNED,
+    IN _selected_AOSO JSON, 
     IN _employee_ids JSON
 )
-BEGIN
+BEGIN    
     -- placeholders
-    DECLARE planned_length_ INT,
+    DECLARE i TINYINT DEFAULT 0;
+    DECLARE planned_length_ INT;
+    DECLARE service_length_id_ INT UNSIGNED;
+    DECLARE employee_id_ INT UNSIGNED;
 
-    -- calculate length
+    -- calculate planned length
     CALL sp_calculate_length(
         _service_id, 
         _employee_id, 
@@ -19,22 +23,34 @@ BEGIN
         planned_length_
     );
 
-    -- Step 1: Create a temporary table (if not already created)
-    CREATE TEMPORARY TABLE DELAs AS
-    SELECT column1, column2
-    FROM your_table
-    WHERE some_condition;
+    -- iterate every employee_id from _employee_ids (json array)
+    WHILE i < JSON_LENGTH(_employee_ids) DO 
+        -- fetch employee_id
+        SET employee_id_ = JSON_UNQUOTE(JSON_EXTRACT(_employee_ids, CONCAT('$[', i, ']')));
+        SET i = i + 1;
 
-    -- Step 2: Check if at least one row exists in the temporary table
-    IF EXISTS (SELECT 1 FROM temp_table LIMIT 1) THEN
-        -- The table is not empty, so perform your SELECT query
-        SELECT * FROM temp_table;
-    ELSE
-        -- The table is empty
-        SELECT 'The table is empty' AS message;
-    END IF;
+        -- fetch DELA via date & employee & planned length
+        CREATE TEMPORARY TABLE DELA AS
+            SELECT ds.slots
+                FROM DELAs d
+                    JOIN DELA_slots ds
+                    ON d.DELA_id = ds.DELA_id
+                WHERE d.date = _date
+                    AND d.employee_id = employee_id_
+                    AND d.planned_length = planned_length_;
 
-    -- Step 3: (Optional) Drop the temporary table when done
-    DROP TEMPORARY TABLE IF EXISTS temp_table;
+        -- check if DELA is empty 
+        IF EXISTS (SELECT 1 FROM DELA LIMIT 1) THEN
+            -- if DELA not empty, return DELA
+            SELECT * FROM DELA;
+        ELSE
+            -- if DELA empty, return list of date-employee appointments & planned length & stored intervals
+            SELECT 'The table is empty' AS message;
+        END IF;
 
+        -- clean up
+        DROP TEMPORARY TABLE IF EXISTS DELA;
+
+        -- end loop
+    END WHILE;
 END;
