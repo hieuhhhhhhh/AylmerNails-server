944fd972eed25687518ef05e4505f47bc998133f
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-01-08 19:23:01 -0500

    c

--- src/routes/appointments/availability/helpers/store_DELA.py
+++ src/routes/appointments/availability/helpers/store_DELA.py
@@ -1,11 +1,10 @@
 import json
 from src.mysql.procedures.call_2D_proc import call_2D_proc
+from src.mysql.procedures.thread_pool import job_queue
 
 
 # send DELA to the mysql server
 def store_DELA(DELA, DELA_id):
-    # convert to json
-    slots = json.dumps(DELA)
 
-    # call mysql proc to process data
-    call_2D_proc("sp_add_DELA_slots", DELA_id, slots)
+    # call mysql proc to process data in another thread
+    job_queue.put(lambda: call_2D_proc("sp_add_DELA_slots", DELA_id, json.dumps(DELA)))
