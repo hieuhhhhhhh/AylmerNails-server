a91dc0229c23b4d2d246055382a379087ac1f6cf
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-01-08 19:08:12 -0500

    c

--- src/routes/appointments/availability/helpers/store_DELA.py
+++ src/routes/appointments/availability/helpers/store_DELA.py
@@ -1,3 +1,11 @@
+import json
+from src.mysql.procedures.call_2D_proc import call_2D_proc
+
+
 # send DELA to the mysql server
 def store_DELA(DELA, DELA_id):
-    return
+    # convert to json
+    slots = json.dumps(DELA)
+
+    # call mysql proc to process data
+    call_2D_proc("sp_add_DELA_slots", DELA_id, slots)
