42101a8bc4c428ab02e0db0ba3f540e20e860447
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-01-16 23:19:17 -0500

    remove group

--- src/mysql/tables/services/category_groups.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE category_groups(
-    group_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE
-);
--- src/routes/services/services/get_all_services.py
+++ src/routes/services/services/get_all_services.py
@@ -10,7 +10,7 @@ def get_all_services():
         jsonify(
             {
                 "all_services": services,
-                "list_definition": "service_id, service_name, category_id, category_name, group_id, group_name",
+                "list_definition": "service_id, service_name, category_id, category_name",
             }
         ),
         200,
