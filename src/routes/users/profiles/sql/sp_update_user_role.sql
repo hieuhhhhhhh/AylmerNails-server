daec148101a29761ec30fce556628b9a12d3b649
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2025-06-28 22:07:54 -0400

    i

--- src/routes/users/profiles/sql/sp_update_user_role.sql
+++ src/routes/users/profiles/sql/sp_update_user_role.sql
@@ -6,8 +6,24 @@ CREATE PROCEDURE sp_update_user_role(
     IN _role VARCHAR(50)
 )
 BEGIN    
+    -- variables
+    DECLARE role_ VARCHAR(50);
+
     -- validate session token
     CALL sp_validate_owner(_session);    
+
+    -- fetch role of user
+    SELECT role
+        INTO role_
+        FROM authentication 
+        WHERE user_id= _user_id;
+
+    -- validate role
+    IF role_ IN ('owner')
+    THEN 
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = '400, Can not change role of owners';
+    END IF;
 
     -- update role
     UPDATE authentication
--- src/routes/users/profiles/update_user_role.py
+++ src/routes/users/profiles/update_user_role.py
@@ -4,6 +4,14 @@ from src.mysql.procedures.call_3D_proc import call_3D_proc
 
 def update_user_role(session, user_id, role):
     # call mysql proc to process data
-    call_3D_proc("sp_update_user_role", session, user_id, role)
+    try:
+        call_3D_proc("sp_update_user_role", session, user_id, role)
+    except Exception as e:
+        status = e.msg[:3]
+
+        if status == "400":
+            return jsonify({"message": "Can not change role of owners"}), 403
+
+        return jsonify({"message": "Only owners can change a user's role"}), 403
 
     return "updated successfully", 200
